4d41bab6e040e83675c7eebe026de17f4d638f4e
Pukii <salima.pusch@gmx.de>
2023-07-15 21:51:25 +0200

    started impl search filters ui

--- src/components/innerComponents/SearchFilters.jsx
+++ src/components/innerComponents/SearchFilters.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import "../../styles/SearchFilters.scss"
+import { useState } from "react"
 
 export default function SearchFilters({setFilters}) {
+  const [movies, setMovies] = useState(true)
+  
+  const toggleMediaType = (e)=>{
+    e.preventDefault();
+    setMovies(e.target.value == 'movies')
+  }
+  
   return (
     <div className="filters">
-      Filters
+      <div className="mediaType">
+        <button 
+          value="movies"
+          onClick={toggleMediaType}
+          className={`mediaButton ${movies ? 'selected' : ''}`}>
+            movies
+        </button>
+        <button 
+          value="shows"
+          onClick={toggleMediaType} 
+          className={`mediaButton ${movies ? '' : 'selected'}`}>
+            shows
+        </button>
+      </div>
     </div>
   )
 }
